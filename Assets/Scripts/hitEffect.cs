9337781921af2b1e966542332b40b504a5ff5c0f
Sttrevens <Sttrevens@126.com>
2024-12-18 16:18:31 +0800

    Better hit effects

--- Assets/Scripts/hitEffect.cs
+++ Assets/Scripts/hitEffect.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class hitEffect : MonoBehaviour
 {
-    public GameObject hitParticles;
+    public GameObject lightHitParticles;
+    public GameObject heavyHitParticles;
+    public int lightDamageAmountThreshold;
     public AudioSource hitSound;
     // Start is called before the first frame update
     void Start()
@@ -16,5 +19,22 @@ public class hitEffect : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Hit(int damageAmount, Vector3 hitPoint, Vector3 hitNormal)
+    {
+        if ((lightHitParticles != null) && (damageAmount > 0 && damageAmount <= lightDamageAmountThreshold))
+        {
+            Destroy(Instantiate(lightHitParticles, hitPoint, Unity.Mathematics.quaternion.LookRotation(hitNormal, Vector3.up)), 10.0f);
+        }
+        else if ((heavyHitParticles != null) && (damageAmount > lightDamageAmountThreshold))
+        {
+            Destroy(Instantiate(heavyHitParticles, hitPoint, Unity.Mathematics.quaternion.LookRotation(hitNormal, Vector3.up)), 10.0f);
+        }
+
+        if (hitSound != null)
+        {
+            hitSound.Play();
+        }
     }
 }
